64105a65357ddac937d6305b1db3ea7d33b64a10
mohan2see <mohan2see@gmail.com>
2020-03-22 23:52:02 -0300

    adding git notes

---
+++ git_basics/git.sh
@@ -0,0 +1,28 @@
+# NOTES : GIT
+# GIT is a version control tool, helps to keep track the changes made in code.
+
+# create a GIT repo in github / other third party providers.
+# configuring github in local
+git config -global user.name "mohan2see"
+git config -global user.email "mohan2see@gmail.com"
+
+# setup local repository. this code will create a folder called `code` and add `.git` folder inside.
+git init code
+
+# add a sub directory and some code
+cd $HOME/code
+mkdir sqoop
+touch sqoop/sqoop.sh
+echo "this is a test code" > sqoop/sqoop.sh
+
+# adding repository files to index. this command will add sqoop folder and sqoop.sh inside it to local repository
+git add sqoop
+
+# committing the changes
+git commit -m "sqoop initial version"
+
+# adding the remote repo
+git remote add origin https://github.com/mohan2see/code.git
+
+# commiting the changes to remote repo. do a pull if you have updated work in remote before doing a push.
+git push origin master
